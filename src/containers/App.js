34ba19e2462dee542006bcdbdb5c6e10f33301f2
christopher <christopher083ade@gmail.com>
2023-07-16 16:03:17 +0700

    change the robot to api

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,19 +6,28 @@ import "./App.css";
 
 class App extends React.Component {
     constructor(){
+       //console.log("constructor");
         super();
         this.state = {
-            robots : robots,
+            robots : [],
             searchfield : "",
             
         }
     }
+    componentDidMount(){
+        //console.log("componentDidMount");
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => {this.setState({robots: users})});
+    }
+
     SearchChange = (event) => {
         this.setState({searchfield: event.target.value});
     }
 
 
     render() {
+        //console.log("render");
         const filteredRobots = this.state.robots.filter((robot) => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());})
         return (
