3b501bf3bbadb8cda6de4f63bc7ded63eb6f7d36
christopher <christopher083ade@gmail.com>
2023-07-16 16:37:15 +0700

    clean the code

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,12 +29,14 @@ class App extends React.Component {
 
     render() {
         //console.log("render");
-        const filteredRobots = this.state.robots.filter((robot) => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());})
-        if (this.state.robots.length === 0){
-            return <h1 className="tc flex items-center justify-center">Loading</h1>
-        } else {
-            return (
+        const { robots, searchfield } = this.state;
+
+        const filteredRobots = robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());})
+
+        return !robots.length ?
+            <h1 className="tc flex items-center justify-center">Loading</h1>
+        : (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox onChange={this.SearchChange}/>
@@ -45,7 +47,7 @@ class App extends React.Component {
         )
     }
   }
-}
+
 
 
 export default App;
